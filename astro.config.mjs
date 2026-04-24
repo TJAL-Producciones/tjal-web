7b0f3e24f1eac2c622911742bae169d1fd969795
TJAL Producciones <tjalproducciones@gmail.com>
2026-04-23 22:06:06 -0300

    RB cambio base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tjal-producciones.github.io',
+  base: '/tjal-web',
   integrations: [sitemap({
     i18n: {
       defaultLocale: 'es',
